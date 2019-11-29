fa8a851c07cec81929663c88cc5966b060c4193c
lukapetrovic3110 <58331163+lukapetrovic3110@users.noreply.github.com>
2019-11-29 17:21:09 +0100

    #prvi_commit

---
+++ StudentskaSluzba/src/main/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Luka ra25-2017
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
