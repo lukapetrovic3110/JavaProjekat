d1059ce4ad8bf917fa42de3e609797aac02c5f16
Luka Petrovic <luka.petrovic@uns.ac.rs>
2019-12-17 00:05:35 +0100

    #updateMainWindow

--- StudentskaSluzba/src/view/MainWindow.java
+++ StudentskaSluzba/src/view/MainWindow.java
@@ -3,21 +3,16 @@ package view;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
-//import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-//import java.io.IOException;
+import java.io.IOException;
 
-//import javax.imageio.ImageIO;
-//import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-//import javax.swing.JMenu;
-//import javax.swing.JMenuBar;
-//import javax.swing.JMenuItem;
-//import javax.swing.KeyStroke;
 
-public class MainWindow  extends JFrame
+public class MainWindow extends JFrame
 {
 	private static final long serialVersionUID = 7661406785264865341L;
+	
 	private static MainWindow instance = null;
 
 	public static MainWindow getInstance() 
@@ -32,14 +27,27 @@ public class MainWindow  extends JFrame
 	public MainWindow()
 	{
 		setTitle("Studentska sluzba");
-		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-		setSize(screenDimension.width / 2, screenDimension.height / 2);
-		setLocationRelativeTo(null);
-		//SetMenuBar();
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
-		this.setIconImage(icon);
+		Toolkit toolKit = Toolkit.getDefaultToolkit();
+		Dimension screenDimension = toolKit.getScreenSize();
+		
+		int screenHeight = screenDimension.height;
+		int screenWidth = screenDimension.width;
+		
+		setSize(screenWidth * 3/4, screenHeight * 3/4);
+		
+		setLocationRelativeTo(null);
+	
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);	
+		
+		try {
+			ImageIcon imageIcon = new ImageIcon(ImageIO.read(getClass().getResourceAsStream("icon_main_window.png")));
+			Image mainWindowImage = imageIcon.getImage();
+			this.setIconImage(mainWindowImage);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 
 		StatusBar statusBar = new StatusBar(this);
 		statusBar.DrawStatusBar();
@@ -47,11 +55,10 @@ public class MainWindow  extends JFrame
 		MenuBar menuBar = new MenuBar(this);
 		menuBar.SetMenuBar();
 		
+		ToolBar toolBar = new ToolBar(this);
+		toolBar.setToolBar();
 		
 		setVisible(true); //poslednja metoda koja se poziva
 	}
-	
-	
-	
 	
 }
