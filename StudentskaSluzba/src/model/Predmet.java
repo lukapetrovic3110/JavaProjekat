9c644327d164cb9ae0a8fa7fbbdd9d1b8b08acd7
Luka Petrovic <luka.petrovic@uns.ac.rs>
2019-12-21 02:53:15 +0100

    #predmet_klasa
    Izmenjena predmet klasa

--- StudentskaSluzba/src/model/Predmet.java
+++ StudentskaSluzba/src/model/Predmet.java
@@ -1,24 +1,41 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * @author ra25-2017
+ */
 public class Predmet {
 	private int sifra_predmeta;
 	private String naziv_predmeta;
 	private String semestar;
-	private String godinastudija;
+	private String godina_studija;
 	private Profesor predmetniProfesor;
-	private ArrayList<Student> slusajustudenti;
-	public int getSifra_predmeta() {
+	private ArrayList<Student> slusajuStudenti;
+	
+	public Predmet() {}
+	
+	public Predmet(int sifra_predmeta, String naziv_predmeta, String semestar,
+				   String godina_studija, Profesor predmetniProfesor, ArrayList<Student> slusajuStudenti) {
+	
+		super();
+		this.sifra_predmeta = sifra_predmeta;
+		this.naziv_predmeta = naziv_predmeta;
+		this.semestar = semestar;
+		this.godina_studija = godina_studija;
+		this.predmetniProfesor = predmetniProfesor;
+		this.slusajuStudenti = slusajuStudenti;
+	}
+	
+	public int getSifrapredmeta() {
 		return sifra_predmeta;
 	}
-	public void setSifra_predmeta(int sifra_predmeta) {
+	public void setSifrapredmeta(int sifra_predmeta) {
 		this.sifra_predmeta = sifra_predmeta;
 	}
-	public String getNaziv_predmeta() {
+	public String getNazivpredmeta() {
 		return naziv_predmeta;
 	}
-	public void setNaziv_predmeta(String naziv_predmeta) {
+	public void setNazivpredmeta(String naziv_predmeta) {
 		this.naziv_predmeta = naziv_predmeta;
 	}
 	public String getSemestar() {
@@ -28,9 +45,33 @@ public class Predmet {
 		this.semestar = semestar;
 	}
 	public String getGodinastudija() {
-		return godinastudija;
+		return godina_studija;
 	}
-	public void setGodinastudija(String godinastudija) {
-		this.godinastudija = godinastudija;
+
+	public void setGodinastudija(String godina_studija) {
+		this.godina_studija = godina_studija;
 	}
+
+	public Profesor getPredmetniProfesor() {
+		return predmetniProfesor;
+	}
+	public void setPredmetniProfesor(Profesor predmetniProfesor) {
+		this.predmetniProfesor = predmetniProfesor;
+	}
+	
+	public ArrayList<Student> getSlusajuStudenti() {
+		return slusajuStudenti;
+	}
+
+	public void setSlusajuStudenti(ArrayList<Student> slusajuStudenti) {
+		this.slusajuStudenti = slusajuStudenti;
+	}
+
+	@Override
+	public String toString() {
+		return "Predmet [sifra_predmeta=" + sifra_predmeta + ", naziv_predmeta=" + naziv_predmeta + ", semestar="
+				+ semestar + ", godina_studija=" + godina_studija + ", predmetniProfesor=" + predmetniProfesor
+				+ ", slusajuStudenti=" + slusajuStudenti + "]";
+	}
+
 }
