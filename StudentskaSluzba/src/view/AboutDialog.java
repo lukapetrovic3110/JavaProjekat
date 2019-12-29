c852d977eaf677dd2168acfd067d6c73d9f7d520
jelenabudisa <jelena.budisa@uns.ac.rs>
2019-12-29 22:53:21 +0100

    #help dialog and about dialog improved

--- StudentskaSluzba/src/view/AboutDialog.java
+++ StudentskaSluzba/src/view/AboutDialog.java
@@ -9,14 +9,22 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 
 public class AboutDialog extends JDialog{
 	private static final long serialVersionUID = 3591599721565020284L;
+	
 	public AboutDialog(JFrame parentFrame,boolean modal) {
 		super(parentFrame,modal);
-		JOptionPane.showMessageDialog(null, "tekst", "About", JOptionPane.PLAIN_MESSAGE);
-	};
+		//JOptionPane.showMessageDialog(null, "tekst", "About", JOptionPane.PLAIN_MESSAGE);
+		String longMessage="dieGFJD\njsidnfuhgyda\nnkafsa\nfghj\ngtfhgjukh\ndfgh\nsd\nfdg\nfgfh\ndf\ndfd\n";
+	    JTextArea textArea = new JTextArea(7, 30);
+	    textArea.setText(longMessage);
+	    textArea.setEditable(false);
+	    JScrollPane scrollPane = new JScrollPane(textArea);
+	    JOptionPane.showMessageDialog(parentFrame, scrollPane);
+	}
 
-
-}	
+};	
--- StudentskaSluzba/src/view/HelpDialog.java
+++ StudentskaSluzba/src/view/HelpDialog.java
@@ -4,14 +4,22 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class HelpDialog extends JDialog{
-	private static final long serialVersionUID = 3591599721565020284L;
+	private static final long serialVersionUID = 359599721565020284L;
 	
 	public HelpDialog(JFrame parentFrame,boolean modal)
 	{
 		super(parentFrame,modal);
-		JOptionPane.showMessageDialog(null, "tekst", "Help", JOptionPane.PLAIN_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "tekst", "Help", JOptionPane.PLAIN_MESSAGE);
+		String longMessage="dieGFJD\njsidnfuhgyda\nnkafsa\nfghj\ngtfhgjukh\ndfgh\nsd\nfdg\nfgfh\ndf\ndfd\n";
+	    JTextArea textArea = new JTextArea(7, 30);
+	    textArea.setText(longMessage);
+	    textArea.setEditable(false);
+	    JScrollPane scrollPane = new JScrollPane(textArea);
+	    JOptionPane.showMessageDialog(parentFrame, scrollPane);
 	}
 	
 }
