7c892ab815176ba059c3fb2aa5414546bf9e9bcd
Luka Petrovic <luka.petrovic@uns.ac.rs>
2019-12-31 17:09:25 +0100

    NewTabbedPane

--- StudentskaSluzba/src/view/TabbedPane.java
+++ StudentskaSluzba/src/view/TabbedPane.java
@@ -1,10 +1,5 @@
 package view;
 
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.JButton;
-
 /**
  *  @author ra25-2017
  */
@@ -18,11 +13,12 @@ public class TabbedPane extends JTabbedPane
 	private static final long serialVersionUID = -5733815749423597587L;
 	
 	private JFrame frame;
-	private ToolBar tBar;
+	public static ToolBar tBar;
 	
-	private PanelProfesori panelProfesori;
-	private PanelPredmeti panelPredmeti;
-	private PanelStudenti panelStudenti;
+	public static PanelProfesori panelProfesori;
+	public static PanelPredmeti panelPredmeti;
+	public static PanelStudenti panelStudenti;
+	public static TabbedPane tabbedPane;
 	
 	public static int ind;
 	
@@ -34,7 +30,7 @@ public class TabbedPane extends JTabbedPane
 	
 	public void setTabbedPane()
 	{
-		TabbedPane tabbedPane = new TabbedPane(frame, tBar);
+		tabbedPane = new TabbedPane(frame, tBar);
 		
 		panelStudenti = new PanelStudenti(frame.getWidth(), frame.getHeight());
 		tabbedPane.add("Studenti", panelStudenti);
@@ -45,67 +41,9 @@ public class TabbedPane extends JTabbedPane
 		panelPredmeti = new PanelPredmeti(frame.getWidth(), frame.getHeight());
 		tabbedPane.add("Predmeti", panelPredmeti);
 		
-		tabbedPane.addMouseListener(new MouseListener() {
+		TabbedPaneMouseListener tabbePaneMouseListener = new TabbedPaneMouseListener();
+		tabbedPane.addMouseListener(tabbePaneMouseListener);
 			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				if(tabbedPane.getSelectedComponent() == panelPredmeti)
-				{
-					JButton btnDodajStudentaNaPredmet = tBar.getBtnNew2();
-					btnDodajStudentaNaPredmet.setVisible(true);
-					btnDodajStudentaNaPredmet.setToolTipText("Dodaj studenta na predmet");
-					
-					JButton btnDodajProfesoraNaPredmet = tBar.getBtnNew3();
-					btnDodajProfesoraNaPredmet.setVisible(true);
-					btnDodajProfesoraNaPredmet.setToolTipText("Dodaj profesora na predmet");
-				}
-				if(tabbedPane.getSelectedComponent() == panelStudenti)
-				{
-					JButton btnDodajStudentaNaPredmet = tBar.getBtnNew2();
-					btnDodajStudentaNaPredmet.setVisible(false);
-					btnDodajStudentaNaPredmet.setToolTipText("Dodaj studenta na predmet");
-					
-					JButton btnDodajProfesoraNaPredmet = tBar.getBtnNew3();
-					btnDodajProfesoraNaPredmet.setVisible(false);
-					btnDodajProfesoraNaPredmet.setToolTipText("Dodaj profesora na predmet");
-				}
-				if(tabbedPane.getSelectedComponent() == panelProfesori)
-				{
-					JButton btnDodajStudentaNaPredmet = tBar.getBtnNew2();
-					btnDodajStudentaNaPredmet.setVisible(false);
-					btnDodajStudentaNaPredmet.setToolTipText("Dodaj studenta na predmet");
-					
-					JButton btnDodajProfesoraNaPredmet = tBar.getBtnNew3();
-					btnDodajProfesoraNaPredmet.setVisible(false);
-					btnDodajProfesoraNaPredmet.setToolTipText("Dodaj profesora na predmet");
-				}
-			}
-		
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		//System.out.println(this.getSelectedIndex());
 		frame.getContentPane().add(tabbedPane);
 	}
 }
