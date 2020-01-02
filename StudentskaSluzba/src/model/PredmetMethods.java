0c8161d2e4438005072a49586e0d985332599155
Luka Petrovic <luka.petrovic@uns.ac.rs>
2020-01-02 14:48:41 +0100

    PredmetMethods

---
+++ StudentskaSluzba/src/model/PredmetMethods.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PredmetMethods {
+	
+	private static ArrayList<Predmet> predmeti = BazaPredmeta.getInstance().getPredmeti();
+	
+	public static void dodajPredmet(String sifraPredmeta, String nazivPredmeta, String semestar, String godinaStudija)
+	{
+	
+		predmeti.add(new Predmet(sifraPredmeta, nazivPredmeta, semestar, godinaStudija));
+	}
+	
+	public static void izbrisiPredmet(String sifraPredmeta)
+	{
+		for(Predmet p : predmeti)
+		{
+			if(p.getSifrapredmeta().equals(sifraPredmeta))
+			{
+				predmeti.remove(p);
+				break;
+			}
+		}
+	}
+	
+	public static void izmeniPredmet(String sifraPredmeta, String nazivPredmeta, String semestar, String godinaStudija)
+	{
+		for(Predmet p : predmeti)
+		{
+			if(p.getSifrapredmeta().equals(sifraPredmeta))
+			{
+				p.setNazivpredmeta(nazivPredmeta);
+				p.setSemestar(semestar);
+				p.setGodinastudija(godinaStudija);
+			}
+		}
+	}
+}
