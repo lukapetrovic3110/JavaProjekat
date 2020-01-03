e527e631706bd5de232ec7ad7e92c1a68dfc9fc6
Luka Petrovic <luka.petrovic@uns.ac.rs>
2020-01-04 00:42:06 +0100

    OptionPaneBrisanjeProfesoraIZMENJENO

--- StudentskaSluzba/src/view/OptionPaneBrisanjeProfesora.java
+++ StudentskaSluzba/src/view/OptionPaneBrisanjeProfesora.java
@@ -3,6 +3,8 @@ package view;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import controller.ProfesorController;
+
 public class OptionPaneBrisanjeProfesora extends JOptionPane{
 	
 	private static final long serialVersionUID = -894509145520418143L;
@@ -11,8 +13,14 @@ public class OptionPaneBrisanjeProfesora extends JOptionPane{
 	{
 		Object[] options = { "Potvrdi", "Odustani" };
 		
-		JOptionPane.showOptionDialog(null, "Da li ste sigurni da zelite da obrisete profesora?", "Brisanje profesora",
+		int n = JOptionPane.showOptionDialog(null, "Da li ste sigurni da zelite da obrisete profesora?", "Brisanje profesora",
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
 				new ImageIcon("src/icons/icon_delete.png"), options, options[1]);
+		
+		if (n == 0)
+		{
+			int rowSelected = PanelProfesori.tableProfesori.getSelectedRow();
+			ProfesorController.getInstance().izbrisiProfesora(rowSelected);
+		}
 	}
 }
