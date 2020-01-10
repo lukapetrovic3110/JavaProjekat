5831166215a73d3c32787f5e57c67a009c0502c8
jelenabudisa <jelena.budisa@uns.ac.rs>
2020-01-11 00:33:43 +0100

    #about dialog

--- StudentskaSluzba/src/view/AboutDialog.java
+++ StudentskaSluzba/src/view/AboutDialog.java
@@ -14,7 +14,14 @@ public class AboutDialog extends JOptionPane{
 	public AboutDialog(JFrame parentFrame) {
 		
 		//JOptionPane.showMessageDialog(null, "tekst", "About", JOptionPane.PLAIN_MESSAGE);
-		String longMessage="dieGFJD\njsidnfuhgyda\nnkafsa\nfghj\ngtfhgjukh\ndfgh\nsd\nfdg\nfgfh\ndf\ndfd\n";
+		String longMessage=" ** Ovo je Java GUI aplikacija sa koristenjem Swing biblioteke, pod nazivom 'Studentska sluzba'.\nAplikacija predstavlja rad sa podacima o studentima,"
+				+ "profesorima i predmetima u nekoj studentskoj sluzbi.\nNjen izgled sastoji se iz glavnog prozora sa trakom sa menijima,\ntrakom sa alatkama,trakom sa izbranim entitetima i statusnom linijom.\n"
+				+ "Pomocu aplikacije je moguce vrsiti dodavanje,izmenu,pretragu i sortiranje datih podatdaka.\n"
+				+ " \n** Autori ove aplikacije su Luka Petrovic i Jelena Budisa.Luka je rodjen 31.10.1998.godine\n u Novom Sadu,Backi Jarak,Republika Srbija."
+				+ "\nZavrsio je Elektrotehnicku skolu 'Mihajlo Pupin' u Novom Sadu.\nSada je student 3.godine Fakulteta tehnickih nauka u Novom Sadu,"
+				+ "\nusmerenje Racunarstvo i automatika."
+				+ "\nJelena je rodjena 12.6.1998.godine u Banjoj Luci,BiH.\nPohadjala je Katolicku gimnaziju u Banjoj Luci \na trenutno je student"
+				+ " 3.godine Fakulteta tehnickih nauka u Novom Sadu,\nusmerenje Racunarstvo i automatika.";
 	    JTextArea textArea = new JTextArea(7, 30);
 	    textArea.setText(longMessage);
 	    textArea.setEditable(false);
