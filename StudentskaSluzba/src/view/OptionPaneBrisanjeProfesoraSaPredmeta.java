11239f5aa2db7dec88fe9018473eed40b8d8e185
Luka Petrovic <luka.petrovic@uns.ac.rs>
2020-01-12 23:34:39 +0100

    #uklanjanje_predmeta_sa_spiska_predmeta_kod_profesora

--- StudentskaSluzba/src/view/OptionPaneBrisanjeProfesoraSaPredmeta.java
+++ StudentskaSluzba/src/view/OptionPaneBrisanjeProfesoraSaPredmeta.java
@@ -1,13 +1,26 @@
 package view;
 
+import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import controller.PredmetController;
+import model.BazaPredmeta;
+import model.BazaProfesora;
+import model.Predmet;
+import model.Profesor;
 
 public class OptionPaneBrisanjeProfesoraSaPredmeta extends JOptionPane{
 	
 	private static final long serialVersionUID = 3497501625586315362L;
+	private Predmet predmet;
+	private String profa;
+	private String podaci[];
+	private String brojLicnekarte;
+	private ArrayList<Profesor> profesori = BazaProfesora.getInstance().getProfesori();
+	private Profesor profesor;
+	private ArrayList<Predmet> predmeti;
 	
 	public OptionPaneBrisanjeProfesoraSaPredmeta() 
 	{
@@ -22,6 +35,36 @@ public class OptionPaneBrisanjeProfesoraSaPredmeta extends JOptionPane{
 		{
 			int rowSelected = PanelPredmeti.tablePredmeti.getSelectedRow();
 			rowSelected = PanelPredmeti.tablePredmeti.convertRowIndexToModel(rowSelected);
+			
+			predmet = BazaPredmeta.getInstance().getRow(rowSelected);
+			profa = predmet.getProfa();
+					
+			podaci = profa.split(",");
+			
+			brojLicnekarte = podaci[0];
+			
+			brojLicnekarte = brojLicnekarte.substring(1, 10);
+		
+			for(Profesor p : profesori)
+			{
+				if(p.getBrlicne().equals(brojLicnekarte))
+				{
+					profesor = p;
+				}
+			}
+			
+			predmeti = profesor.getSpisakpredmeta();
+			
+			for(int i = 0; i < predmeti.size(); i++)
+			{
+				if(predmeti.get(i).getSifrapredmeta().equals(predmet.getSifrapredmeta()))
+				{
+					predmeti.remove(i);
+					PanelProfesori.azurirajPrikaz();
+				}
+			
+			}
+			
 			PredmetController.getInstance().obrisiProfesoraSaPredmeta(rowSelected);
 		}	
 	}
