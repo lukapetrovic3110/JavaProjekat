b4d1b61154c35f710bd848499150ec47422d98fd
Luka Petrovic <luka.petrovic@uns.ac.rs>
2020-01-12 19:58:18 +0100

    OptionPaneBrisanjeProfesoraSaPredmeta

---
+++ StudentskaSluzba/src/view/OptionPaneBrisanjeProfesoraSaPredmeta.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import controller.PredmetController;
+
+public class OptionPaneBrisanjeProfesoraSaPredmeta extends JOptionPane{
+	
+	private static final long serialVersionUID = 3497501625586315362L;
+	
+	public OptionPaneBrisanjeProfesoraSaPredmeta() 
+	{
+		Object[] options = { "Potvrdi", "Odustani" };	
+
+		int n = JOptionPane.showOptionDialog(null, "Da li ste sigurni da zelite da obrisete profesora sa predmeta?", "Brisanje profesora sa predmeta",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+				new ImageIcon("src/icons/icon_delete.png"), options, options[1]);
+
+	
+		if (n == 0)
+		{
+			int rowSelected = PanelPredmeti.tablePredmeti.getSelectedRow();
+			PredmetController.getInstance().izbrisiPredmet(rowSelected);
+		}	
+	}
+}
